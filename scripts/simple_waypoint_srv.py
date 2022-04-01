9a95d231ea45e24b8b480da01bec8d05f43df1d5
ciaran13 <ciaran@helgen.tech>
2022-04-01 11:22:40 +0000

    testing appending waypoints

--- scripts/simple_waypoint_srv.py
+++ scripts/simple_waypoint_srv.py
@@ -1,27 +1,32 @@
 #!/usr/bin/env python
 import rospy
 import time
-from waypoint_navigator.srv import GoToWaypoint
+from waypoint_navigator.srv import GoToWaypoints
 from geometry_msgs.msg import Point
 
-def call_service(waypoint):
+def call_service(waypoints):
   rospy.wait_for_service('/firefly/go_to_waypoint')
   try:
-    go_to_waypoint = rospy.ServiceProxy('/firefly/go_to_waypoint', GoToWaypoint)
-    go_to_waypoint.call(waypoint)
+    go_to_waypoints = rospy.ServiceProxy('/firefly/go_to_waypoints', GoToWaypoints)
+    go_to_waypoints.call(waypoints)
   except rospy.ServiceException as e:
     print("Service call failed: %s"%e)
 
 if __name__ == '__main__':
 
-  point = Point(x=0.0, y=0.0, z=1.0)
-  points = [Point(x=0.0, y=0.0, z=1.0), Point(x=1, y=0.0, z=1.0), Point(x=1, y=1, z=1.0), Point(x=0, y=1, z=1.0)]
+  points1 = [Point(x=0, y=0, z=2.0), Point(x=-1, y=-1, z=2.0)]
+  points2 = [Point(x=-2, y=-2, z=2.0), Point(x=0.0, y=0.0, z=2.0)]
   # publish estimate at 1 Hz
   # Loop to publish pose estimate. Subscriber callbacks continue in background
+  called = 0
   while not rospy.is_shutdown():
       try:
-        for point in points:
-          call_service(point)
-          time.sleep(6)
+        if called == 0:
+          call_service(points1)
+          time.sleep(2)
+
+          call_service(points2)
+          called = 1
+        
       except rospy.ROSInterruptException:
           pass
